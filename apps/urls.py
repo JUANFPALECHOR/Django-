53bdb2ddca68f52c0bbcf77a3adc57eb447d9b1f
JUANFPALECHOR <juanfelipepalechor@gmail.com>
2024-03-06 11:17:34 -0500

    agg el login_required

--- apps/urls.py
+++ apps/urls.py
@@ -13,12 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
+from apps.views import bienvenido, base,inicio,contenido
 
 urlpatterns = [
-    path ('',include('apps.urls')),
-    path('admin/', admin.site.urls),
-    path ('accounts/', include('django.contrib.auth.urls')),
+
+    path('bienvenido/',bienvenido,name='bienvenido'),
+    path('base/',base),
+    path ('',inicio,name= 'inicio' ),
+    path ('contenido/',contenido,name='contenido'),
+
 ]
--- apps/views.py
+++ apps/views.py
@@ -3,22 +3,27 @@ from django.shortcuts import render
 
 from personas.models import Persona
 
+from django.contrib.auth.decorators import  login_required
+
 
 # Create your views here.
 
+
+def base(request):
+    return render(request,"base.html")
 def bienvenido(request):
     personas = Persona.objects.all()
     return render(request, 'bienvenido.html',{'personas':personas})# Renderiza la plantilla 'bienvenido.html' y devuelve la respuesta HTML al usuario
 
-def casa(request):
-    return render(request,'casa.html')
 
-def despedida(request):
-    return render(request,"base.html")
+def inicio(request):
+    return render(request,'inicio.html')
 
-
+@login_required
 def contenido(request):
     return render(request, "contenido.html")
+
+
 
 
 
